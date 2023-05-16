ca2ff164d8b7b3a6b42207b1880593772a437b50
Aidan Bell <aidanbell0182@gmail.com>
2023-05-16 12:40:51 -0400

    fix mobile

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -6,37 +6,19 @@ export default function Home() {
     <div id="home" className="home">
       <div id="particles-js">
         <div className="home-container">
-          <p className="name">/ab/</p>
-          <div className="mobile-nav">
-            <ul className="home-nav">
-              <li>
-                <a className="mobile-nav-link" href="#About">
-                  <span>About Me</span>
-                </a>
-              </li>
-              <li>
-                <a className="mobile-nav-link" href="#Projects">
-                  <span>Projects</span>
-                </a>
-              </li>
-              <li>
-                <a className="mobile-nav-link" href="#Contact">
-                  <span>Contact</span>
-                </a>
-              </li>
-            </ul>
-          </div>
+          <p className="name outline">/ab/</p>
         </div>
       </div>
+      <a href="#about" id="down">
         <i
           className="material-icons"
-          id="down"
-          style={{ fontSize: "10vmin", cursor: "pointer" }}
+          
           onClick={() => {
             this.props.scroll(this.props.scrollRef);
           }}>
           arrow_downward
         </i>
+        </a>
     </div>
   );
 }
